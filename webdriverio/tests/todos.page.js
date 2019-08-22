e597c02189ea20d3ce03ec7cfa313b3a368060fa
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-08-22 11:09:34 -0400

    First webdriverIO test

--- webdriverio/tests/todos.page.js
+++ webdriverio/tests/todos.page.js
@@ -4,6 +4,18 @@ class TodosPage {
     return $('.new-todo')
   }
 
+  get todoItems() {
+    return $$('[qa-id=todo-item')
+  }
+
+  get todoCount() {
+    return $('.todo-count')
+  }
+
+  addNewTodo(text) {
+    this.newTodoInput.setValue(text + "\uE007")
+  }
+
 }
 
 export const todosPage = new TodosPage()
--- webdriverio/tests/todos.test.js
+++ webdriverio/tests/todos.test.js
@@ -1,11 +1,24 @@
 import { todosPage } from './todos.page'
+import * as todos from '../../cypress/fixtures/activeTodos.json'
 
 describe('Todos tests', () => {
+
+  beforeEach(() => {
+    browser.execute((todos) => {
+      fetch('/api/todos/bulkload', {
+        method: 'POST',
+        body: { todos: todos },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    }, todos)
+  })
 
   it('display an input box', () => {
     browser.url('/')
     console.log(todosPage.newTodoInput)
     todosPage.newTodoInput.waitForDisplayed()
-    browser.pause(5000)
+    todosPage.addNewTodo('Something')
   })
 })
